200af74e735ce4f0088bfb68e65922f70766afc9
Michael Hadorn <michael.hadorn@laupercomputing.com>
2014-11-21 06:56:59 +0100

    add live example (for download)

---
+++ 2-live/lib.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var zustand = true;
+var zustand2 = false;
+
+zustand2 = (1+1 == 3);
+
+if (1+1 == 3) {
+  console.log("der Zustand ist wahr");
+} else {
+  console.log("der Zustand ist NICHT wahr");
+}
